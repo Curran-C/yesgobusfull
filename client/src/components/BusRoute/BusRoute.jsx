5042169b16092dd8c46a22001a0a952b6845cdc1
= <=>
2023-09-02 14:12:58 +0530

    editable inputs added in busbooking page

--- client/src/components/BusRoute/BusRoute.jsx
+++ client/src/components/BusRoute/BusRoute.jsx
@@ -1,17 +1,40 @@
+import { useState } from "react";
 import { twowayarrow } from "../../assets/busbooking";
 import BusRouteCard from "../BusRouteCard/BusRouteCard";
 import Button from "../Button/Button";
 import "./BusRoute.scss";
 
 const BusRoute = ({ locationOne, locationTwo, departureDate, returnDate }) => {
+  const [LocationOne, setLocationOne] = useState(locationOne);
+  const [LocationTwo, setLocationTwo] = useState(locationTwo);
+  const [DepartureDate, setDepartureDate] = useState(departureDate);
+  const [ReturnDate, setReturnDate] = useState(returnDate);
+
   return (
     <div className="BusRoute">
-      <BusRouteCard title="From" location={locationOne} />
+      <BusRouteCard
+        title="From"
+        location={LocationOne}
+        setLocation={setLocationOne}
+      />
       <img src={twowayarrow} alt="" />
-      <BusRouteCard title="To" location={locationTwo} />
-      <BusRouteCard title="Selected Date" location={departureDate} />
-      <BusRouteCard title="Selected Date" location={departureDate} />
-      <BusRouteCard title="Return Optional" location={returnDate} />
+      <BusRouteCard
+        title="To"
+        location={LocationTwo}
+        setLocation={setLocationTwo}
+      />
+      <BusRouteCard
+        title="Selected Date"
+        location={DepartureDate}
+        setLocation={setDepartureDate}
+        date={true}
+      />
+      <BusRouteCard
+        title="Return Optional"
+        location={ReturnDate}
+        setLocation={setReturnDate}
+        date={true}
+      />
       <Button text={"Search"} />
     </div>
   );
