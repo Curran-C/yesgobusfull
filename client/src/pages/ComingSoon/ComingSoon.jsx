9cb0673f19a780053a28bebe29faf33f09a644d3
Adithya <adithyahebbar32@gmail.com>
2023-09-22 18:01:19 +0530

    kyc page to load slower

--- client/src/pages/ComingSoon/ComingSoon.jsx
+++ client/src/pages/ComingSoon/ComingSoon.jsx
@@ -2,13 +2,17 @@ import { Button, Footer, Navbar } from "../../components";
 import "./ComingSoon.scss";
 import comingSoonImg from "../../assets/comingSoon/comingSoon.png";
 import { useNavigate } from "react-router-dom";
-
+import { Spin } from "antd";
+import { useEffect, useState } from "react";
 
 function ComingSoon() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
   const redirectToCabDriverLogin = () => {
-    // window.location.href = "https://yesgobusnew.web.app/kyc"
-    navigate("/cabs/kyc")
+    setIsLoading(true);
+    setTimeout(() => {
+      navigate("/cabs/kyc");
+    }, 1000);
   }
   return (
     <section className="coming_soon">
@@ -17,11 +21,11 @@ function ComingSoon() {
         <img src={comingSoonImg} alt="coming soon" />
       </div>
       <div className="buttons">
-  <div className="button-container">
-    <Button text={"Be a Cab Driver"} onClicked={redirectToCabDriverLogin} />
-  </div>
-</div>
-      
+        <div className="button-container">
+          <Button text={"Be a Cab Driver"} onClicked={redirectToCabDriverLogin} />
+        </div>
+      </div>
+
       {/* <h6>Site under construction</h6> */}
       {/* <div className="subscribe">
         <span className="input_container">
@@ -35,6 +39,11 @@ function ComingSoon() {
         </span>
       </div> */}
       <Footer />
+      {isLoading && (
+        <div className="loading-screen">
+          <Spin size="large" tip="Loading..." />
+        </div>
+      )}
     </section>
   );
 }
